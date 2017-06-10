45c4275e1ded20c6fe9e356e3e8618f3da35588c
tomrutherford <tmrutherford@gmail.com>
2017-06-10 13:50:00 -0700

    Implement recall util

--- metafunctions/tests/test_util.py
+++ metafunctions/tests/test_util.py
@@ -7,8 +7,19 @@ from metafunctions import util
 
 class TestIntegration(BaseTestCase):
     def test_store(self):
+        abc = a | b | util.store('output') | c
 
-        self.fail()
+        self.assertEqual(abc('_'), '_abc')
+        self.assertEqual(abc.data['output'], '_ab')
+
+    def test_recall(self):
+        a.data['k'] = 'secret'
+
+        cmp = a + b | util.store('k') | c + util.recall('k')
+        self.assertEqual(cmp('_'), '_a_bc_a_b')
+
+        cmp = a + b | util.store('k') | c + util.recall('k') | util.recall('k', from_meta=a)
+        self.assertEqual(cmp('_'), 'secret')
 
 @node
 def a(x):
--- metafunctions/util.py
+++ metafunctions/util.py
@@ -1,12 +1,26 @@
 '''
 Utility functions for use in function pipelines.
 '''
-from . import decorators
+from metafunctions.decorators import node
+from metafunctions.metafunctions import MetaFunction
 
 
 def store(key):
     '''Store the received output in the meta data dictionary under the given key.'''
+    @node(bind=True)
     def f(meta, val):
-        meta[key] = val
+        meta.data[key] = val
         return val
+    return f
+
+
+def recall(key, from_meta:MetaFunction=None):
+    '''Retrieve the given key from the meta data dictionary. Optionally, use `from_meta` to specify
+    a different metafunction than the current one.
+    '''
+    @node(bind=True)
+    def f(meta, val):
+        if from_meta:
+            return from_meta.data[key]
+        return meta.data[key]
     return f
