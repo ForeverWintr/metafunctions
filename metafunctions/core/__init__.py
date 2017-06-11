0c8d991c1c785983c5e5d7d8981bf9ca1d7e4ae1
tomrutherford <tmrutherford@gmail.com>
2017-06-11 14:45:43 -0700

    Convert core.py to core package

---
+++ metafunctions/core/__init__.py
@@ -0,0 +1,10 @@
+'''
+Everything within core that isn't exposed here should be considered internal and subject to
+change
+'''
+
+from ._base import MetaFunction
+from ._base import SimpleFunction
+from ._base import FunctionChain
+from ._base import FunctionMerge
+from ._base import DeferredValue
