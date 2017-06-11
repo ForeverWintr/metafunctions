c192b7ef8aee862415d4dc59ee87ea0137f08016
tomrutherford <tmrutherford@gmail.com>
2017-06-11 00:54:39 -0700

    Add long_description to setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,20 @@
-
+import os
 from setuptools import setup, find_packages
 
 import metafunctions
+
+
+here = os.path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name=metafunctions.__name__,
     version=metafunctions.__version__,
     description='Metafunctions is a function composition and data pipelining library',
+    long_description=long_description,
     url='https://github.com/ForeverWintr/metafunctions',
     author='Tom Rutherford',
     license='MIT',
