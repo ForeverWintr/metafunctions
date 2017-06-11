242775ae473dd217704257eea0956650e83d7e3e
tomrutherford <tmrutherford@gmail.com>
2017-06-11 11:36:54 -0700

    Setup.py converts md to rst

--- setup.py
+++ setup.py
@@ -1,32 +1,56 @@
 import os
+import contextlib
+import pathlib
 from setuptools import setup, find_packages
+
+import pandoc
 
 import metafunctions
 
 
 here = os.path.abspath(os.path.dirname(__file__))
 
-# Get the long description from the README file
-with open(os.path.join(here, 'README.md')) as f:
-    long_description = f.read()
+@contextlib.contextmanager
+def md_to_rst(dir_):
+    '''Convert anything with a .md extension to .rst for the duration of this context manager.
+    '''
+    dir_ = pathlib.Path(dir_)
+    rstfiles = []
+    for mdfile in dir_.glob('*.md'):
+        doc = pandoc.Document()
+        doc.markdown = mdfile.read_bytes()
+        rstfile = mdfile.with_suffix('.rst')
+        rstfile.write_bytes(doc.rst)
+        rstfiles.append(rstfile)
 
-setup(
-    name=metafunctions.__name__,
-    version=metafunctions.__version__,
-    description='Metafunctions is a function composition and data pipelining library',
-    long_description=long_description,
-    url='https://github.com/ForeverWintr/metafunctions',
-    author='Tom Rutherford',
-    license='MIT',
-    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.6',
-    ],
-    keywords='functional-programming function-composition',
-    packages=find_packages(),
-    test_suite='metafunctions.tests',
-)
+    yield rstfiles
+    for f in rstfiles:
+        f.unlink()
+
+
+with md_to_rst(here):
+    with open(os.path.join(here, 'README.rst')) as f:
+        long_description=f.read()
+
+
+    setup(
+        name=metafunctions.__name__,
+        version=metafunctions.__version__,
+        description='Metafunctions is a function composition and data pipelining library',
+        long_description=long_description,
+        url='https://github.com/ForeverWintr/metafunctions',
+        author='Tom Rutherford',
+        author_email='tom.rutherford@alumni.uleth.ca',
+        license='MIT',
+        # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        classifiers=[
+            'Development Status :: 3 - Alpha',
+            'Intended Audience :: Developers',
+            'Topic :: Software Development :: Libraries :: Python Modules',
+            'License :: OSI Approved :: MIT License',
+            'Programming Language :: Python :: 3.6',
+        ],
+        keywords='functional-programming function-composition',
+        packages=find_packages(),
+        test_suite='metafunctions.tests',
+    )
