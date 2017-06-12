1266fda07f0379d99a14b595bfa2d88b58527634
tomrutherford <tmrutherford@gmail.com>
2017-06-11 22:45:21 -0700

    Add highlight_current_function
    To show where in a metafunction the current function is.

--- metafunctions/tests/test_util.py
+++ metafunctions/tests/test_util.py
@@ -3,7 +3,7 @@ from metafunctions.tests.util import BaseTestCase
 from metafunctions.decorators import node
 from metafunctions import util
 
-class TestIntegration(BaseTestCase):
+class TestUnit(BaseTestCase):
     def test_store(self):
         abc = a | b | util.store('output') | c
 
@@ -18,6 +18,28 @@ class TestIntegration(BaseTestCase):
 
         cmp = a + b | util.store('k') | c + util.recall('k') | util.recall('k', from_meta=a)
         self.assertEqual(cmp('_'), 'secret')
+
+    def test_highlight_current_function(self):
+        fmt_index = 6
+
+        @node
+        def ff(x):
+            return x + 'F'
+
+        @node(bind=True)
+        def f(meta, x):
+            if len(meta._called_functions) == 6:
+                location_string = util.highlight_current_function(meta)
+                location_strin_color = util.highlight_current_function(meta, use_color=True)
+                self.assertEqual(location_string, '(a | b | ff | f | f | ->f<- | f | f)')
+                self.assertEqual(location_strin_color,
+                        '(a | b | ff | f | f | \x1b[31m->f<-\x1b[0m | f | f)')
+                self.assertEqual(x, '_abFff')
+            return x + str(f)
+
+        pipe = a | b | ff | f | f | f | f | f
+        pipe('_')
+
 
 @node
 def a(x):
--- metafunctions/util.py
+++ metafunctions/util.py
@@ -1,6 +1,11 @@
 '''
 Utility functions for use in function pipelines.
 '''
+import sys
+import re
+
+import colors
+
 from metafunctions.decorators import node
 from metafunctions.core import MetaFunction
 
@@ -24,3 +29,42 @@ def recall(key, from_meta:MetaFunction=None):
             return from_meta.data[key]
         return meta.data[key]
     return f
+
+
+def system_supports_color():
+    """
+    Returns True if the running system's terminal supports color, and False
+    otherwise.
+    """
+    plat = sys.platform
+    supported_platform = plat != 'Pocket PC' and (plat != 'win32' or
+                                                  'ANSICON' in os.environ)
+    # isatty is not always implemented, #6223.
+    is_a_tty = hasattr(sys.stdout, 'isatty') and sys.stdout.isatty()
+    if not supported_platform or not is_a_tty:
+        return False
+    return True
+
+
+def highlight_current_function(meta, color=colors.red, use_color=system_supports_color()):
+    '''Return a formatted string showing the location of the currently active function in meta.
+
+    Consider this a 'you are here' when called from within a function pipeline.
+    '''
+    current_index = len(meta._called_functions)
+    current_name = str(meta._called_functions[-1])
+
+    # how many times will current_name appear in str(meta)?
+    # Bearing in mind that pervious function names may contain current_name
+    num_occurences = sum(str(f).count(current_name) for f in meta._called_functions)
+
+
+    highlighted_name = f'->{current_name}<-'
+    if use_color:
+        highlighted_name = color(highlighted_name)
+
+    regex = f'^((.*?{current_name}.*?){{{num_occurences-1}}}){current_name}'
+    highlighted_string = re.sub(regex, fr'\1{highlighted_name}', str(meta))
+    return highlighted_string
+
+
