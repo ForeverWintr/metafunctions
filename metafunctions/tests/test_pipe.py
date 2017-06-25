0a63825d152092d74bf93df179321417d550a66a
tomrutherford <tmrutherford@gmail.com>
2017-06-25 13:33:42 -0700

    Remove temporary workspace

--- metafunctions/tests/test_pipe.py
+++ metafunctions/tests/test_pipe.py
@@ -215,33 +215,3 @@ def d(x):
 def e(x):
     return x + 'e'
 
-if __name__ == '__main__':
-    class bcolors:
-        HEADER = '\033[95m'
-        OKBLUE = '\033[94m'
-        OKGREEN = '\033[92m'
-        WARNING = '\033[93m'
-        FAIL = '\033[91m'
-        ENDC = '\033[0m'
-        BOLD = '\033[1m'
-        UNDERLINE = '\033[4m'
-
-    import sys
-
-
-    def supports_color():
-        """
-        Returns True if the running system's terminal supports color, and False
-        otherwise.
-        """
-        plat = sys.platform
-        supported_platform = plat != 'Pocket PC' and (plat != 'win32' or
-                                                      'ANSICON' in os.environ)
-        # isatty is not always implemented, #6223.
-        is_a_tty = hasattr(sys.stdout, 'isatty') and sys.stdout.isatty()
-        if not supported_platform or not is_a_tty:
-            return False
-        return True
-    print(f"Does this system support colour? {'yes' if supports_color() else 'no'}")
-
-    raise Exception(f'{bcolors.UNDERLINE}{bcolors.FAIL}hi! {bcolors.BOLD} this shouldn\'t be coloured')
