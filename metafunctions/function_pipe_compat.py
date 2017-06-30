5cd992ac3ff2f63dcd5b5da0a6a6e69d6fcc99bb
tomrutherford <tmrutherford@gmail.com>
2017-06-30 16:43:09 -0700

    Import some function-pipe stuff

--- metafunctions/function_pipe_compat.py
+++ metafunctions/function_pipe_compat.py
@@ -1,5 +1,21 @@
 '''This module adapts the MetaFunction interface to the function-pipe interface.
 '''
+import function_pipe as _fpn
 
-def pipe_node(_func=None ):
-    return _func
+from metafunctions.util import node
+
+# PipeNode kwargs
+PREDECESSOR_RETURN = _fpn.PREDECESSOR_RETURN
+PREDECESSOR_PN = _fpn.PREDECESSOR_PN
+PN_INPUT = _fpn.PN_INPUT
+PN_INPUT_SET = {PN_INPUT}
+PIPE_NODE_KWARGS = {PREDECESSOR_RETURN, PREDECESSOR_PN, PN_INPUT}
+
+def pipe_node(_func=None):
+    return node(func)
+
+
+def pipe_kwarg_bind(*key_positions):
+    def f(func):
+        return func
+    return f
--- metafunctions/tests/test_function_pipe_compat.py
+++ metafunctions/tests/test_function_pipe_compat.py
@@ -1,4 +1,4 @@
-SimpleFunction
+
 from metafunctions.tests.util import BaseTestCase
 
 import metafunctions.function_pipe_compat as fpn
@@ -7,3 +7,9 @@ class TestUnit(BaseTestCase):
 
     def test_string_processing(self):
         # examples from https://function-pipe.readthedocs.io/en/latest/usage_strings.html
+        @fpn.pipe_node
+        @fpn.pipe_kwarg_bind(fpn.PREDECESSOR_RETURN)
+        def a(s):
+            return s + 'a'
+
+        self.assertEqual(a(**{fpn.PN_INPUT: '_'}), '_a')
