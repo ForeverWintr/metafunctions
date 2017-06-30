b431ff46a51ccbdb2da794b4629e66e26da66c96
tomrutherford <tmrutherford@gmail.com>
2017-06-30 16:04:09 -0700

    Initial skeleton of function-pipe compatibility

---
+++ metafunctions/function_pipe_compat.py
@@ -0,0 +1,5 @@
+'''This module adapts the MetaFunction interface to the function-pipe interface.
+'''
+
+def pipe_node(_func=None ):
+    return _func
---
+++ metafunctions/tests/test_function_pipe_compat.py
@@ -0,0 +1,9 @@
+SimpleFunction
+from metafunctions.tests.util import BaseTestCase
+
+import metafunctions.function_pipe_compat as fpn
+
+class TestUnit(BaseTestCase):
+
+    def test_string_processing(self):
+        # examples from https://function-pipe.readthedocs.io/en/latest/usage_strings.html
