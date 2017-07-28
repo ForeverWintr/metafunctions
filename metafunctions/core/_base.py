17a7e2d6f8420607b90a769dd28183ec2a19e0df
tomrutherford <tmrutherford@gmail.com>
2017-07-27 20:40:30 -0700

    Access all operators through operators module

--- metafunctions/core/_base.py
+++ metafunctions/core/_base.py
@@ -1,4 +1,3 @@
-import operator
 import typing as tp
 import abc
 import itertools
@@ -8,7 +7,7 @@ import functools
 from metafunctions.core._decorators import binary_operation
 from metafunctions.core._decorators import inject_call_state
 from metafunctions.core._call_state import CallState
-from metafunctions.operators import concat
+from metafunctions import operators
 from metafunctions import exceptions
 
 
@@ -63,43 +62,43 @@ class MetaFunction(metaclass=abc.ABCMeta):
 
     @binary_operation
     def __and__(self, other):
-        return FunctionMerge.combine(concat, self, other)
+        return FunctionMerge.combine(operators.concat, self, other)
 
     @binary_operation
     def __rand__(self, other):
-        return FunctionMerge.combine(concat, other, self)
+        return FunctionMerge.combine(operators.concat, other, self)
 
     @binary_operation
     def __add__(self, other):
-        return FunctionMerge(operator.add, (self, other))
+        return FunctionMerge(operators.add, (self, other))
 
     @binary_operation
     def __radd__(self, other):
-        return FunctionMerge(operator.add, (other, self))
+        return FunctionMerge(operators.add, (other, self))
 
     @binary_operation
     def __sub__(self, other):
-        return FunctionMerge(operator.sub, (self, other))
+        return FunctionMerge(operators.sub, (self, other))
 
     @binary_operation
     def __rsub__(self, other):
-        return FunctionMerge(operator.sub, (other, self))
+        return FunctionMerge(operators.sub, (other, self))
 
     @binary_operation
     def __mul__(self, other):
-        return FunctionMerge(operator.mul, (self, other))
+        return FunctionMerge(operators.mul, (self, other))
 
     @binary_operation
     def __rmul__(self, other):
-        return FunctionMerge(operator.mul, (other, self))
+        return FunctionMerge(operators.mul, (other, self))
 
     @binary_operation
     def __truediv__(self, other):
-        return FunctionMerge(operator.truediv, (self, other))
+        return FunctionMerge(operators.truediv, (self, other))
 
     @binary_operation
     def __rtruediv__(self, other):
-        return FunctionMerge(operator.truediv, (other, self))
+        return FunctionMerge(operators.truediv, (other, self))
 
     @binary_operation
     def __matmul__(self, other):
@@ -147,11 +146,11 @@ class FunctionChain(MetaFunction):
 
 class FunctionMerge(MetaFunction):
     _character_to_operator = {
-        '+': operator.add,
-        '-': operator.sub,
-        '*': operator.mul,
-        '/': operator.truediv,
-        '&': concat,
+        '+': operators.add,
+        '-': operators.sub,
+        '*': operators.mul,
+        '/': operators.truediv,
+        '&': operators.concat,
     }
     _operator_to_character = {v: k for k, v in _character_to_operator.items()}
 
--- metafunctions/operators.py
+++ metafunctions/operators.py
@@ -1,6 +1,7 @@
 '''
 Extra operators used by MetaFunctions
 '''
+from operator import add, sub, truediv, mul
 
 def concat(*args):
     "concat(1, 2, 3) -> (1, 2, 3)"
