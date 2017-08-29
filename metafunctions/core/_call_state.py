e1b2703534f3e83d6ca10c75afd8b1eee4ecd118
tomrutherford <tmrutherford@gmail.com>
2017-08-28 20:01:31 -0700

    Add call_tree and tests

--- metafunctions/core/_call_state.py
+++ metafunctions/core/_call_state.py
@@ -1,3 +1,4 @@
+from collections import namedtuple, defaultdict, OrderedDict
 
 class CallState:
     __slots__ = (
@@ -21,3 +22,52 @@ class CallState:
         if exception is not self._exception:
             self._exception = exception
             self._exception_meta_stack = tuple(self._meta_stack)
+
+
+class CallTree:
+    Node = namedtuple('Node', 'function insert_index')
+    def __init__(self):
+        '''
+        A call tree keeps track of function execution order in metafunctions. This is used to
+        accurately determine the location of the currently active function, and to identify
+        exception locations. It can be thought of as a metafunction aware call stack.
+        '''
+        # Meta entry is conceptually the root node of the call tree
+        self._meta_entry = None
+        self._active_node = None
+        self._nodes_visited = 0
+
+        # A dictionary of {child: parent} functions
+        self._bottom_up = OrderedDict()
+
+        # A dictionary of {parent: [children]}, in call order
+        self._horizontal = defaultdict(list)
+
+    def push(self, f):
+        '''
+        Push a function onto the tree
+        '''
+        if self._meta_entry is None:
+            node = self.Node(f, self._nodes_visited)
+            self._meta_entry = node
+        else:
+            node = self.Node(f, self._nodes_visited)
+            self._bottom_up[node] = self._active_node
+            self._horizontal[self._active_node].append(node)
+        self._nodes_visited += 1
+        self._active_node = node
+
+    def pop(self):
+        '''Remove last inserted f from the call tree.'''
+        try:
+            node, parent = self._bottom_up.popitem()
+        except KeyError:
+            m = self._meta_entry
+            self._meta_entry = None
+            self._active_node = None
+            return m
+        self._horizontal.pop(node, None)
+        self._active_node = parent
+        return node[0]
+
+
---
+++ metafunctions/tests/test_call_tree.py
@@ -0,0 +1,47 @@
+from metafunctions.tests.util import BaseTestCase
+from metafunctions.api import bind_call_state
+from metafunctions.api import node
+from metafunctions.core._call_state import CallState, CallTree
+
+
+class TestUnit(BaseTestCase):
+    def test_add_remove(self):
+        # imagine these are functions
+        a, b, c, d = 'a b c d'.split()
+        Node = CallTree.Node
+
+        tree = CallTree()
+        tree.push(a)
+        tree.push(a)
+        self.assertEqual(tree.pop(), a)
+        self.assertDictEqual(tree._bottom_up, {})
+
+        tree.push(b)
+        tree.push(a)
+        self.assertDictEqual(tree._bottom_up,
+                             {Node(b, 2): Node(a, 0),
+                              Node(a, 3): Node(b, 2)})
+        self.assertDictEqual(tree._horizontal,
+                             {Node(a, 0): [Node(a, 1), Node(b, 2)],
+                              Node(b, 2): [Node(a, 3)],})
+
+        tree.push(b)
+        self.assertDictEqual(tree._horizontal,
+                             {Node(a, 0): [Node(a, 1), Node(b, 2)],
+                              Node(b, 2): [Node(a, 3)],
+                              Node(a, 3): [Node(b, 4)],})
+        self.assertDictEqual(tree._bottom_up,
+                             {Node(b, 4): Node(a, 3),
+                              Node(a, 3): Node(b, 2),
+                              Node(b, 2): Node(a, 0),})
+
+        self.assertEqual(tree.pop(), b)
+        self.assertEqual(tree.pop(), a)
+        for f in a, b, c, d:
+            tree.push(f)
+            tree.pop()
+        self.assertDictEqual(tree._horizontal,
+                {Node(a, 0): [Node(a, 1), Node(b, 2)],
+                 Node(b, 2): [Node(a, 3), Node(a, 5), Node(b, 6), Node(c, 7), Node(d, 8)]})
+        self.assertDictEqual(tree._bottom_up, {Node(b, 2): Node(a, 0),})
+
