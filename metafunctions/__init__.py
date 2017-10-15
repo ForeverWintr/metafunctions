26340df030b3bfd2b1f9864699445f07a9629094
tomrutherford <tmrutherford@gmail.com>
2017-10-15 15:15:36 -0700

    Version 1.0

--- metafunctions/__init__.py
+++ metafunctions/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.3.0'
+__version__ = '1.0.0'
 
 from metafunctions.api import (node, bind_call_state, star, store, recall, concurrent, mmap,
                                locate_error)
