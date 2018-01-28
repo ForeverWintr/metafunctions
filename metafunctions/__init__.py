8a0749cc3025bec75c4bec4c829735207ee6cd59
tomrutherford <tmrutherford@gmail.com>
2018-01-27 18:50:21 -0800

    Bump version

--- metafunctions/__init__.py
+++ metafunctions/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '1.0.1'
+__version__ = '1.1.0'
 
 from metafunctions.api import (node, bind_call_state, star, store, recall, concurrent, mmap,
                                locate_error)
