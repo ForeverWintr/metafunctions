d410b98313976dab50c0f0f0890d8ed97b428089
tomrutherford <tmrutherford@gmail.com>
2022-04-13 19:58:14 -0600

    Bump version and note support for python 3.11

--- setup.py
+++ setup.py
@@ -73,6 +73,7 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
     ],
     keywords="functional-programming function-composition",
     packages=find_packages(),
